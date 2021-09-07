df2c73e44c2c374f0e134a107fbc1a1efec29dcd
pavankumarchebelli <76152858+pavankumarchebelli@users.noreply.github.com>
2021-09-08 01:44:32 +0530

    First commit before http

---
+++ src/app/components/todo-item/todo-item.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Todo } from 'src/app/models/Todo';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+
+  @Input() 
+  todo: Todo = new Todo;
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  //Set Dynamic Classes
+  setClasses(){
+    let classes={
+      todo: true,
+      'is-complete': this.todo.completed
+    }
+
+    return classes;
+  }
+
+  onToggle(todo: any){
+    todo.completed=!todo.completed;
+  }
+
+  onDelete(todo: any){
+    console.log('delete');
+  }
+}
